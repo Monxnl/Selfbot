646e66cbb92e140a9d879f1fadfe8867ac64beaf
Monx <87898962+Monxnl@users.noreply.github.com>
2024-09-26 17:15:58 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,24 +9,5 @@ const r = express.Router()
 			delete global._qr
 		} else res.redirect('/')
 	})
-	/*
-	.get('/nowa', async (req, res) => {
-		let q = req.query.number, regex = /x/g
-		if (!q) return res.json({ message: 'Input parameter number' })
-		if (!q.match(regex)) return res.json({ message: 'Parameter number must fill with one letter "x"' })
-		
-		let random = q.match(regex).length, total = Math.pow(10, random), arr = []
-		for (let i = 0; i < total; i++) {
-			let list = [...i.toString().padStart(random, '0')]
-			let result = q.replace(regex, () => list.shift()).replace(/\D/g, '') + '@s.whatsapp.net'
-			if (((await conn.onWhatsApp(result))[0] || {}).exists) {
-				let info = await conn.fetchStatus(result).catch(_ => _)
-				if (info.setAt) info.setAt = +info.setAt ? info.setAt : 'Invalid Date'
-				arr.push({ jid: result, exists: true, ...info })
-			} else arr.push({ jid: result, exists: false })
-		}
-		res.json(arr)
-	})
-	*/
-
+	
 module.exports = r
